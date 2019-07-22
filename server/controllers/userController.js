53e4ff4e62830def424492bcbf0e02b4f99e2d9a
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-07-22 09:41:21 -0600

    Completed userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcrypt")
+const saltRounds = 15
+
+module.exports = {
+    async login(req, res) {
+        let { username, password } = req.body
+        const db = req.app.get('db')
+        let [existingUser] = await db.get_user_by_username(username)
+        if (!existingUser) return res.status(401).send('Username not found')
+        let result = await bcrypt.compare(password, existingUser.password)
+        if (result) {
+            req.session.user = {
+                username: existingUser.username,
+                id: existingUser.id,
+                loggedIn: true
+            }
+            res.send(req.session.user)
+        } else res.status(401).send('Incorrect Username or Password')
+    },
+    async signup(req, res) {
+        let { username, password } = req.body
+        const db = req.app.get('db')
+        let [existingUser] = await db.get_user_by_username(username)
+        if (existingUser) return res.status(400).send('Username Already Exists')
+        let salt = await bcrypt.genSalt(saltRounds)
+        let hash = await bcrypt.hash(password, salt)
+        let [user] = await db.create_user([username, hash])
+        req.session.user = { username: user.username, id: user.id, loggedIn: true }
+        res.send(req.session.user)
+    },
+    logout(req, res) {
+        req.session.destroy()
+        res.sendStatus(200)
+    },
+    getUser(req, res) {
+        res.send(req.session.user)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require('dotenv').config({ path: __dirname + '/../.env'})
 const express = require('express')
 const massive = require('massive')
+const session = require('express-session')
 const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
 
 const app = express()
 app.use(express.json())
+
+app.use(session({
+    secret: SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365
+    }
+}))
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db',db)
