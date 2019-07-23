a2ce3169f64808a03d9ed4ab31b81d08ea96f8bb
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-07-23 11:36:51 -0600

    Morning Upload 07/23/19

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,4 +6,4 @@ const rootReducer = combineReducers({
     user: userReducer
 })
 
-export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
+export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(promiseMiddleware)))
